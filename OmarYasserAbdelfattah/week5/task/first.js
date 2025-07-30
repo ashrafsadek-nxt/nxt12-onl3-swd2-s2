0ff037866276ab76648368f315abbbfc7be61f9a
Omar Yasser <oy30977@gmail.com>
2025-07-30 15:51:33 +0300

    Add files via upload

---
+++ OmarYasserAbdelfattah/week5/task/first.js
@@ -0,0 +1,113 @@
+document.getElementById("head").innerText = "programming language espcial for frontend";
+document.getElementById('dv').innerHTML = "<h5>Loose programming language</h5>";
+document.getElementById('ipt').value = 123;
+document.write("<br>welcome javascript");
+console.log(1234);
+
+var x = 10;
+console.log(x);
+
+var x = 30;
+console.log(x);
+
+x = "ooo"; 
+console.log(x);
+
+x = 22.33;
+console.log(x);
+
+let y = 20; 
+console.log(y);
+
+y = 230;    
+console.log(y);
+
+y = "Omar Yasser";
+console.log(y);
+let $y = 20;
+console.log($y);
+let _y = 20;
+console.log(_y);
+let g_y4 = 20;
+console.log(_y);
+const z = "omarr";  
+console.log(z); 
+
+let dd = '22';
+console.log(dd);
+console.log(typeof(dd));
+
+dd = 22;
+console.log(dd);
+console.log(typeof(dd));
+
+dd = 22.345;
+console.log(dd);
+console.log(typeof(dd));
+
+dd = true;
+console.log(dd);
+console.log(typeof(dd));
+
+dd = false;
+console.log(dd);
+console.log(typeof(dd));
+
+dd = null;
+console.log(dd);
+console.log(typeof(dd));
+
+dd = undefined;
+console.log(dd);
+console.log(typeof(dd));
+
+dd = Symbol('f');
+console.log(dd);
+console.log(typeof(dd));
+
+dd = [12,"oooo", true, 34.45];
+console.log(dd);
+console.log(typeof(dd));
+dd = Array(12,"oooo", true, 34.45);
+console.log(dd);
+console.log(typeof(dd));
+
+dd = {
+    age: 13,
+    name: "omar"
+}
+console.log(dd);
+console.log(typeof(dd));
+
+dd = function z (){
+    let x = 0;
+}
+console.log(dd);
+console.log(typeof(dd));
+
+dd = z => {10+20}
+console.log(dd);
+console.log(typeof(dd));
+
+const input = document.getElementById('ipt2');
+console.log(input);
+console.log(typeof(input));
+
+let num = input.value;
+console.log(num);
+console.log("type of num is ",typeof(num));
+
+const para = document.getElementById('First');
+console.log(para);
+console.log(typeof(para));
+
+let total = num + 10;
+
+para.innerText = total;
+
+const secondPara = document.getElementById('Second');
+
+let sum = parseInt(num) + 10;
+secondPara.innerText = sum;
+console.log(parseInt("omar"));
+console.log(typeof(parseInt("omar")));
