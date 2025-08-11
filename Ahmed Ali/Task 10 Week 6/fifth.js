6eaf48825166fd860016f5a25514bac03168afeb
vhmedali <mokaelnagar00@gmail.com>
2025-08-11 14:44:19 +0300

    Add files via upload

---
+++ Ahmed Ali/Task 10 Week 6/fifth.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('fbtn');
+const section = document.getElementById('sec');
+btn.addEventListener( "click", showhere );
+function showhere()
+{
+    
+    document.getElementById('sec').style.display = "flex";
+}
+
