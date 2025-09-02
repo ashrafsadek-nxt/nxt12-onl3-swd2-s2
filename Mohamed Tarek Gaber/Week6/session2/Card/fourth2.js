96239ff280ec80598920fb1d5ec02481896f879c
Mohamed Tarek <mohamedtarekk663@gmail.com>
2025-09-02 17:16:36 +0300

    Add files via upload

---
+++ Mohamed Tarek Gaber/Week6/session2/Card/fourth2.js
@@ -0,0 +1,9 @@
+
+const button = document.getElementById('btn');
+button.addEventListener('click', showData);
+
+function showData() {
+    const container = document.getElementById("container");
+    container.style.display = "flex";
+}
+
