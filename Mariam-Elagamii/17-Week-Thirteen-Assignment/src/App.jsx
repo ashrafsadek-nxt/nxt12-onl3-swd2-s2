3287ae137dbe0d7b60a3bd26a6f4ee36324807c0
mariam elagamii <110563173+mariamelagamii@users.noreply.github.com>
2025-11-30 22:14:04 +0200

    Implement routing in App component

---
+++ Mariam-Elagamii/17-Week-Thirteen-Assignment/src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import NotFound from "./pages/NotFound";
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
